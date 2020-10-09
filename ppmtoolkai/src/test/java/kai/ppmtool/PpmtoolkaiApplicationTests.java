a1ea3526b0da3e81e820cac8b247479f34561794
kaikai0324 <hk291935258@gmail.com>
2020-10-09 14:05:55 -0400

    first

---
+++ ppmtoolkai/src/test/java/kai/ppmtool/PpmtoolkaiApplicationTests.java
@@ -0,0 +1,13 @@
+package kai.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolkaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
